a9fe4cb3d6f1aacac7a614fc85db02a84b365436
brandon.milambo <brandon.milambo@autotrader.co.uk>
2020-01-17 13:46:32 +0000

    start

---
+++ app/src/main/java/com/example/Directory/model/Business.kt
@@ -0,0 +1,15 @@
+package com.example.Directory.model
+
+data class Business(
+    val id: Int,
+    val name: String,
+    val about:String,
+    val logo: String,
+    var verified: Boolean,
+    val website:String,
+
+    val address: String
+// TBC
+
+
+    )
