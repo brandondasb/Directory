23c08b1d55ac46794a4a758bed9d2105db96852e
brandon.milambo <brandon.milambo@autotrader.co.uk>
2020-08-13 16:05:01 +0100

    current aim :    1. Category onclick listener to open search fragment and populate it with a filtered list.    2. Search to open results fragments with all results.
      number 1 attempt:
       added Result presenter
       added results fragment
       added ViewHolder
    
       not why but the filtered list no longer gets passed though to the onclick.
    
       removed baseFragment,  I believe it was only used to keep track on the onclick, Changed logic a little.
       I believe it still works the same.

--- app/src/main/java/com/melaninwall/directory/presenter/ResultFragmentPresenter.kt
+++ app/src/main/java/com/melaninwall/directory/presenter/ResultFragmentPresenter.kt
@@ -2,8 +2,43 @@ package com.melaninwall.directory.presenter
 
 import android.view.View
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.melaninwall.directory.R
+import com.melaninwall.directory.adapters.ResultRecyclerViewAdapter
+import com.melaninwall.directory.interfaces.ListItemListener
+import com.melaninwall.directory.interfaces.SearchListingCallBack
 import com.melaninwall.directory.model.ListingItemData
+import com.melaninwall.directory.view.ListingFragment
+import com.melaninwall.directory.viewHolder.ResultFragmentViewHolder
 
-class ResultFragmentPresenter(view: View, fragmentManager: FragmentManager?) {
+class ResultFragmentPresenter(itemView: View, var fragmentManager: FragmentManager?) :
+    ListItemListener, SearchListingCallBack {
+    private val context = itemView.context
+
+    private val resultFragmentViewHolder = ResultFragmentViewHolder(itemView)
+    private var resultRecyclerViewAdapter = ResultRecyclerViewAdapter(context, this)
+
+
+    init {
+        val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        resultFragmentViewHolder.resultRecyclerView.layoutManager = linearLayoutManager
+
+        resultRecyclerViewAdapter = ResultRecyclerViewAdapter(context, this)
+    }
+
+    override fun loadItemData(listingItemData: List<ListingItemData>) {
+        resultRecyclerViewAdapter.setData(listingItemData)
+    }
+
+    override fun launchFragment(itemData: ListingItemData) {
+        val listingFragment = ListingFragment.create(itemData)
+
+        // lunch frag
+        fragmentManager?.beginTransaction()
+            ?.replace(R.id.fragment_container, listingFragment)
+            ?.addToBackStack(ListingFragment::class.java.simpleName)
+            ?.commitAllowingStateLoss()
+    }
 
 }
