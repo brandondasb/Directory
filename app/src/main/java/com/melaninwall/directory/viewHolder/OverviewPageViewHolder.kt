1338d8902654763573ba38c42f00ac460cd2b5bf
brandon.milambo <brandon.milambo@autotrader.co.uk>
2020-12-11 07:41:20 +0000

    wip

--- app/src/main/java/com/melaninwall/directory/viewHolder/OverviewPageViewHolder.kt
+++ app/src/main/java/com/melaninwall/directory/viewHolder/OverviewPageViewHolder.kt
@@ -1,6 +1,9 @@
 package com.melaninwall.directory.viewHolder
 
+import android.content.Intent
+import android.net.Uri
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.melaninwall.directory.R
@@ -10,26 +13,50 @@ class OverviewPageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     PageViewGenerator {
 
 
-    val website: TextView? = itemView.findViewById(R.id.overviewWebsite)
-    val twitter: TextView? = itemView.findViewById(R.id.overviewTwitter)
-    val facebook: TextView? = itemView.findViewById(R.id.overviewFacebook)
-    val instagram: TextView? = itemView.findViewById(R.id.overviewInstagram)
     val address: TextView = itemView.findViewById(R.id.listingAddress)
     val city: TextView? = itemView.findViewById(R.id.listingCity)
     val postcode: TextView? = itemView.findViewById(R.id.listingPostcode)
 
+    val twitter: ImageView? = itemView.findViewById(R.id.overviewTwitterImageView)
+    val website: ImageView? = itemView.findViewById(R.id.overviewWebsiteImageView)
+    val instagram: ImageView? = itemView.findViewById(R.id.overviewInstagramImageView)
+    val facebook: ImageView? = itemView.findViewById(R.id.overviewFacebook)
+
     override fun getViews(data: ListingItemData?) {
-        website?.text = data?.website
-        facebook?.text = data?.social?.firstOrNull { it.name == "facebook" }?.url
-        instagram?.text = data?.social?.firstOrNull { it.name == "instagram" }?.url
-        twitter?.text = data?.social?.firstOrNull { it.name == "twitter" }?.url
         address.text = data?.address
         city?.text = data?.city
         postcode?.text = data?.postcode
+
+        website?.setOnClickListener {
+            val link = data?.website
+            openURL(it, link)
+        }
+
+        instagram?.setOnClickListener {
+            val link = data?.social?.firstOrNull { social -> social.name == "instagram" }?.url
+            openURL(it, link)
+        }
+        twitter?.setOnClickListener { view ->
+            data?.social?.firstOrNull { social -> social.name == "twitter" }?.url?.let {
+                openURL(view, it)
+            }
+        }
+        facebook?.setOnClickListener { view ->
+            data?.social?.firstOrNull { social -> social.name == "facebook" }?.url?.let {
+                openURL(view, it)
+            }
+        }
     }
 
-    fun clickListener(view: View, data: ListingItemData) {
-
-
+    private fun openURL(view: View, link: String?) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        var fullUrl = link
+        fullUrl.let {
+            if (!(fullUrl?.startsWith("http://") == true || fullUrl?.startsWith("https://") == true)) {
+                fullUrl = "http://$link"
+            }
+            intent.data = Uri.parse(fullUrl)
+            view.context.startActivity(intent)
+        }
     }
 }
